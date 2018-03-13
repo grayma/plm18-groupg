64c4349f917b3485eda9800504d71368f04a5e08
wjglas <wjglas@ncsu.edu>
2018-03-12 23:31:09 -0400

    Crazy 8s implementation (with a few hiccups)

--- bartok.py
+++ bartok.py
@@ -4,21 +4,84 @@ from random import shuffle
 #==============================================================================
 # Standard Deck
 #==============================================================================
+
 suits = ['hearts', 'diamonds', 'spades', 'clubs']
+abbr_suits = ["h", "d", "s", "c"]
 values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
 
 
 def get_deck():
     return [Card(value, suit) for value in values for suit in suits]
 
+
+#==============================================================================
+# board helpers
+#==============================================================================
+
+def game_status(player, state):
+    print("\nTurn " + str(state[STATE_TURNS]))
+    print("Showing %s info about the game." % (player.name))
+    printBoard(state)
+    print(player.hand)
+    print()  # separator line
+    
+def printBoard(state):
+    print("-----------------")
+    print("|      %d %s      |" % (state['players'][0].score, state['players'][0].name[0]))
+    print("|               |")
+    print("|    top: %s     |" % state["topCard"])
+    print("|               |")
+    print("|      %s %d      |" % (state['players'][1].name[0], state['players'][1].score))
+    print("-----------------")
+
+
 #==============================================================================
 # move functions
 #==============================================================================
 
 def play(player, state, input):
-    return ""
+    card = getCard(input["card"])
+    if not card:
+        return "Invalid input."
+    top_card = state["topCard"]
+    discard = state["discard"]
+    
+    if not card in player.hand:
+        return "Card not in hand."
+    else:
+        if card.value == 8:
+            discard += [card, top_card] #This might not work as intended
+            player.hand.remove(card)
+            print(discard) #Debugging helper to make sure discard was added to correctly
+            valid = false
+            while not valid:
+                # choose a new suit
+                new_suit = input("Choose a new suit (s, c, d, h): ")
+                if new_suit in abbr_suits:
+                    state["topCard"] = Card("", new_suit)
+                    valid = True
+                else:
+                    print("Invalid suit, try again.\n")
+            return ""
+        elif card.suit[0] == top_card.suit[0] or card.value[0] == top_card.value[0]:
+            discard += [top_card]
+            top_card = card
+            player.hand.remove(card)
+            return ""
+        else:
+            return "Card does not match top card."
 
 def draw(player, state, input):
+    deck = state["deck"]
+    
+    if (not deck): #empty
+        deck = shuffle(state["discard"])
+        state["discard"] = []
+        
+    popped = deck.pop();
+    print("You drew: %s" % popped)
+    player.hand += [popped]
+    
     return ""
 
 def placeholder(player, state, input):
@@ -28,7 +91,7 @@ def placeholder(player, state, input):
 # moves
 #==============================================================================
 
-draw_move = Move("draw", draw, { "Nothing (press enter)" })
+draw_move = Move("draw", draw, {})
 play_move = Move("play", play, {"card": None})
 placeholder_move = Move("placeholder", placeholder, {})
 
@@ -46,7 +109,7 @@ def reset_game(state):
     shuffle(deck)
     shuffle(deck)
     shuffle(deck)
-    state["players"][:] = []
+    state["players"][:].hand = []
     deal(game.game_state, deck)
     game.game_state["topCard"] = deck.pop()
     game.game_state["deck"] = deck
@@ -57,13 +120,14 @@ def conclude_game(state):
     print("Final Score:")
     [print("%s: %d" % (p.name, p.score)) for p in state["players"]]
 
+
 #==============================================================================
 # transitions
 #==============================================================================
 
 # len(state[STATE_PLAYERS][0].hand) == 0 or len(state[STATE_PLAYERS][0].hand) == 0
 main_to_main = Transition("main", (lambda state: not any(len(p.hand) == 0 for p in state["players"])), transition_stub)
-main_to_main_reset = Transition("start",
+main_to_main_reset = Transition("main",
        (lambda state: 
             any(len(p.hand) == 0 for p in state["players"]) and get_highest_score(state[STATE_PLAYERS]) < 100),
        reset_game)
@@ -79,18 +143,7 @@ main = State("main", main_transitions, main_moves, game_status, False)
 
 finish = State("finish", None, [placeholder_move], game_status, True)
 
-states = [start, main, finish]
-
-#==============================================================================
-# board helpers
-#==============================================================================
-
-def game_status(player, state):
-    print("\nTurn " + str(state[STATE_TURN]))
-    print("Showing %s info about the game." % (player.name))
-    printBoard(state)
-    print(player.hand)
-    print()  # separator line
+states = [main, finish]
 
 
 #==============================================================================
@@ -98,7 +151,7 @@ def game_status(player, state):
 #==============================================================================
 
 players = []
-for i in range(1, 5):
+for i in range(1, 3):
     name = input("What is the name of player " + str(i) + "? ")
     hand = []
     player = Player(name, {}, i, hand, 0)
@@ -115,9 +168,10 @@ def setup(game):
     shuffle(deck)
     shuffle(deck)
     deal(game.game_state, deck)
-    game.game_state[STATE_CURRENT_STATE] = start
+    game.game_state[STATE_CURRENT_STATE] = main
     game.game_state["topCard"] = deck.pop()
     game.game_state["deck"] = deck
+    game.game_state["discard"] = []
     print("Starting Crazy 8s!\n")
 
 
@@ -129,19 +183,16 @@ def finish(game):
 # helper methods
 #==============================================================================
 
-"""
-player is the player object that *lost* the hand, since he is the only one that will have points added
-"""
-def score_hand(player):
+def score_hand(player_won, player_lost):
     score = 0
-    for c in player.hand:
+    for c in player_lost.hand:
         if c.val == 8:
             score = score + 50
         elif isinstance(c.val, str):
             score = score + 10
         else:
             score = score + c.val
-    player.score = player.score + score
+    player_won.score = player_won.score + score
 
 def deal(state, deck):
     state[STATE_PLAYERS][0].hand.extend(deck[0:8])
@@ -167,4 +218,5 @@ def get_highest_score(players):
 #==============================================================================
 # start the game
 #==============================================================================
-bartok = new Game(players, { "discard" : [] }, )
+bartok = Game(players, {}, states, setup, finish)
+bartok.start()
