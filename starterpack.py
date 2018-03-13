e3342ddc9ef91b8d1186c780936d940e22a0ac12
wjglas <wjglas@ncsu.edu>
2018-03-13 12:16:45 -0400

    Fixed bug in starterpack

--- starterpack.py
+++ starterpack.py
@@ -242,4 +242,4 @@ class Game:
                     trans.pre_transition_logic(self.game_state)
                     self.game_state[STATE_CURRENT_STATE] = self.states[trans.next_state]
 
-        self.finish()
+        self.finish(self)
